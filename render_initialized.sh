2165fce27cd378c46f9bdf4bae5eaee71b5425b1
Tomáš Zítka <tozitka@gmail.com>
2021-05-29 17:42:31 +0200

    Properly rename.

--- render_initialized.sh
+++ render_initialized.sh
@@ -14,25 +14,26 @@
 
 # -- tested by:
 # // commands used in interactive mode to test script start
-## qsub -I -l select=1:ncpus=1:ngpus=1:scratch_ssd=20gb -l walltime=1:00:00 -q gpu
+## qsub -I -l select=1:ncpus=1:ngpus=1:mem=10gb:scratch_ssd=20gb -l walltime=1:00:00 -q gpu
 
 
 # -- actual run
 trap 'clean_scratch' TERM EXIT
 
 module add anaconda3-2019.10
-source activate /storage/plzen1/home/zitkat/.conda/envs/Clef_TBT_env || exit $LINENO
+source activate Clef_TBT_env || exit $LINENO
 
 #mkdir "$SCRATCHDIR"/data || exit $LINENO
-DATA_PATH=$SCRATCHDIR/data
-cp -r /storage/plzen1/home/zitkat/DNNExploratory/data "$SCRATCHDIR" || exit $LINENO
+cp -r /storage/plzen1/home/zitkat/DNNExploratory/ "$SCRATCHDIR" || exit $LINENO
+DATA_PATH=$SCRATCHDIR/DNNExploratory/data
+WORK_PATH=$SCRATCHDIR/DNNExploratory/
 
-cd /storage/plzen1/home/zitkat/DNNExploratory || exit $LINENO
+cd "$WORK_PATH" || exit $LINENO
 today=$(date +%Y%m%d%H%M)
 python render_timm_model.py seresnext50_32x4d -w initialized  -sv v1 \
                             --output "$DATA_PATH" \
                             --hide-progress \
-                            --settings-file "$DATA_PATH"/settings.ods > today.log
-cp -r "$DATA_PATH" /storage/plzen1/home/zitkat/DNNExplorer/data || exit $LINENO
+                            --settings-file "$DATA_PATH"/settings.ods > "$DATA_PATH"/"$today".log
+cp -ru "$DATA_PATH" /storage/plzen1/home/zitkat/DNNExplorer/data || exit $LINENO
 
 
